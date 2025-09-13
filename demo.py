f33eb4f67b7f0c080c6130b97302dcec3928f339
udaygupta8899 <udaygupta8899@gmail.com>
2025-09-13 12:58:46 +0530

    Data Ingestion added

--- demo.py
+++ demo.py
@@ -17,3 +17,7 @@
 # except Exception as e:
 #     logging.info(e)
 #     raise MyException(e, sys) from e
+from src.pipeline.training_pipeline import TrainPipeline
+
+pipeline = TrainPipeline()
+pipeline.run_pipeline()
